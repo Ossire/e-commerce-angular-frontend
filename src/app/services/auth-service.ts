d6ad93f3ecece21f82ce9040f5e69751a9a8c7ee
Ossire <keeerosky@gmail.com>
2026-04-21 13:05:34 +0100

    fixed env trailing slash bug

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ErrorService } from './error-service';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  env = environment;
   private isAuthenticated = new BehaviorSubject<boolean>(this.hasToken());
   public isAuthenticated$: Observable<boolean> = this.isAuthenticated.asObservable();
 
@@ -18,31 +20,27 @@ export class AuthService {
   ) {}
 
   logIn(payload: any): Observable<any> {
-    return this.http
-      .post<any>('https://e-commerce-backend-nestjs-3nac.onrender.com/api/v1/auth/login', payload)
-      .pipe(
-        tap((response) => {
-          localStorage.setItem('access_token', response.access_token);
-          localStorage.setItem('userEmail', payload.email);
-          this.isAuthenticated.next(true);
-        }),
-        catchError((error) => {
-          return this.errorService.handleError(error);
-        }),
-      );
+    return this.http.post<any>(`${this.env.apiUrl}/api/v1/auth/login`, payload).pipe(
+      tap((response) => {
+        localStorage.setItem('access_token', response.access_token);
+        localStorage.setItem('userEmail', payload.email);
+        this.isAuthenticated.next(true);
+      }),
+      catchError((error) => {
+        return this.errorService.handleError(error);
+      }),
+    );
   }
 
   signUp(payload: any): Observable<any> {
-    return this.http
-      .post<any>('https://e-commerce-backend-nestjs-3nac.onrender.com/api/v1/auth/signup', payload)
-      .pipe(
-        tap((response) => {
-          alert('User registration succesful, you can now login');
-        }),
-        catchError((error) => {
-          return this.errorService.handleError(error);
-        }),
-      );
+    return this.http.post<any>(`${this.env.apiUrl}/api/v1/auth/signup`, payload).pipe(
+      tap((response) => {
+        alert('User registration succesful, you can now login');
+      }),
+      catchError((error) => {
+        return this.errorService.handleError(error);
+      }),
+    );
   }
 
   get loginStatus() {
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   prodution: false,
-  apiUrl: 'https://e-commerce-backend-nestjs-3nac.onrender.com/',
+  apiUrl: 'https://e-commerce-backend-nestjs-3nac.onrender.com',
   categoryUrl: 'https://e-commerce-backend-nestjs-3nac.onrender.com/categories',
 };
