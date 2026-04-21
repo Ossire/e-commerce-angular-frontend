c0a19fa7570ab0bc8a90ff0aa379033f7114a8e7
Ossire <keeerosky@gmail.com>
2026-04-21 11:51:35 +0100

    feature: deployed to render

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   prodution: false,
-  apiUrl: 'http://localhost:3000/api/v1/products',
-  categoryUrl: 'http://localhost:3000/api/v1/categories',
+  apiUrl: 'https://e-commerce-backend-nestjs-3nac.onrender.com/',
+  categoryUrl: 'https://e-commerce-backend-nestjs-3nac.onrender.com/categories',
 };
